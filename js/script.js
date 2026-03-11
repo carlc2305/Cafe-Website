7fbc066430bae58b518a4d7efeac10ee1617cd89
carlc2305 <carlchatfield5@gmail.com>
2026-03-11 00:02:23 +0000

    Added a form to the contact page with relevant css and javascript feedback

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Thank you! Your message has been sent.");
+});
